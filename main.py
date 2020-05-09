c2063174fade2d50472c7d984a5e00159d664a4d
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-05-09 18:10:03 +0100

    Update main.py
    Made a check to ensure that the required files exist.
    If the txt file for the w2v file does not exist then create it using the bin file.

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from classifyReddit import runClassifiers
 from classifyTestData import validate
+from gensim.models.keyedvectors import KeyedVectors
 
 
 def main():
     choice = ""
+    try:
+        f = open('word2vec_twitter_tokens.txt')
+        f.close()
+    except FileNotFoundError:
+        print("Pre-loading w2v model! Will take 15-20 mins...")
+        model = KeyedVectors.load_word2vec_format('word2vec_twitter_tokens.bin', binary=True, unicode_errors='ignore')
+        model.save_word2vec_format('word2vec_twitter_tokens.txt', binary=False)
+
+    try:
+        f = open('word2vec_twitter_tokens.bin')
+        f.close()
+        f = open('trainingdata-all-annotations.txt')
+        f.close()
+        f = open('testdata-taskA-all-annotations.txt')
+        f.close()
+    except FileNotFoundError:
+        print("Unable to find the datasets or the word2vec bin file in the directory. Please check if they exist.")
+        return
+
     models = ['Naive Bayes', 'Logistic Regression', 'Support Vector Machine']
 
     for i in range(len(models)):
         print(str(i+1) + ') ' + models[i])
     while choice < '1' or choice > '3':
         choice = input("Choose a model with numbers.")
-    validate(choice) #########
+    validate(choice)
     print("Validation completed! Onto testing...")
-    runClassifiers(choice) #########
+    runClassifiers(choice)
 
-
-main()
+if __name__ == "__main__":
+    main()
