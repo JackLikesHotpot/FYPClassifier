f0c81c85543cd1673899e976ee8c9a67199d56a0
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-05-10 04:05:21 +0100

    Update generateSubmission.py
    comments

--- generateSubmission.py
+++ generateSubmission.py
@@ -2,29 +2,29 @@ import praw
 from praw.models import MoreComments
 
 
-# Generates a Reddit submission of at least 50 comments and analyses the comments.
+# Generates a Reddit submission of at least 100 comments and analyses the comments.
 def generateSub():
-    counter = 0
-    subComments = []
+    counter = 0                                                 # keep a track of how many comments
+    subComments = []                                                        
 
     reddit = praw.Reddit(client_id='SJN5UEqC9iaFHg', client_secret='KYw-MSxoypI3EezUab-V4X_qAO4',
-                         user_agent='FYP-RedditClassifier by /u/Frothpot')
-    submission = reddit.subreddit('worldnews').random()
+                         user_agent='FYP-RedditClassifier by /u/Frothpot')              # api details
+    submission = reddit.subreddit('worldnews').random()                                 # get a random submission from /r/worldnews
 
-    while submission.comments.__len__() < 100:
-        #submission = reddit.submission(id='')
-        submission = reddit.subreddit('worldnews').random()
+    while submission.comments.__len__() < 100:                         
+        #submission = reddit.submission(id='')                  
+        submission = reddit.subreddit('worldnews').random()             # select another submission until it has at least 100 comments
 
-    redditTitle = submission.title
+    redditTitle = submission.title                                      # get the title of the submission
     sub = reddit.submission(id=submission)
 
-    for comment in sub.comments:
-        if counter == 100:
+    for comment in sub.comments:                                        # iterate through each comment
+        if counter == 100:                                              # break if 100 comments
             break
-        if isinstance(comment, MoreComments):
+        if isinstance(comment, MoreComments):                           # if it is a comment object then continue
             continue
         if comment.author != 'autotldr' and comment.author != 'AutoModerator' and comment.body != '[deleted]' and comment.body != '[removed]':
-            subComments.append(comment.body)
+            subComments.append(comment.body)                            # ignore specific comments
             counter += 1
 
-    return redditTitle, subComments
+    return redditTitle, subComments                                     # return the title of the submission and comments
