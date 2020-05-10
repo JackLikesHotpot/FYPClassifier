3e58d5fd5cb30644ea4113b092956d54445b219a
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-05-10 03:52:02 +0100

    Update classifyTestData.py
    comments

--- classifyTestData.py
+++ classifyTestData.py
@@ -9,7 +9,6 @@ from options import processData
 from prepareW2V import prepareW2V
 from readProcessFiles import breakFileIntoStrings
 
-
 def validate(modelChoice):
 
     model = loadGodin()
@@ -27,25 +26,25 @@ def validate(modelChoice):
         trainData, trainStances = processData("trainingdata-all-annotations.txt", features)
         testData, testStances = processData("testdata-taskA-all-annotations.txt", features)
 
-        if features[3] == True:
-            X_train =prepareW2V(model, breakFileIntoStrings('trainingdata-all-annotations.txt')) #godin model takes strings
+        if features[3] == True:                                 # if embeddings option, use embeddings model
+            X_train =prepareW2V(model, breakFileIntoStrings('trainingdata-all-annotations.txt')) 
             X_test = prepareW2V(model, breakFileIntoStrings('testdata-taskA-all-annotations.txt'))
 
-        else:
+        else:                                                   # otherwise don't set up for embeddings
             X_train = vectorizer.fit_transform(trainData)
             X_test = vectorizer.transform(testData)
 
-        if modelChoice == '1':
+        if modelChoice == '1':                                  # train and evaluate bayes
             bayesPred = runBayes(X_train, trainStances, X_test)
             evalBayes(bayesPred, testStances, features)
             print()
 
-        elif modelChoice == '2':
+        elif modelChoice == '2':                                # train and evaluate log reg
             logRegPred = runLogReg(X_train, trainStances, X_test)
             evalLogReg(logRegPred, testStances, features)
             print()
 
-        elif modelChoice == '3':
+        elif modelChoice == '3':                                # train and evaluate svm
             svmPred = runSVM(X_train, trainStances, X_test)
             evalSVM(svmPred, testStances, features)
             print()
