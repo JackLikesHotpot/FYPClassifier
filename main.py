b4906236590013c631e1303008ff15309cba3a1e
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-05-10 02:49:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
         f.close()
     except FileNotFoundError:
         print("Pre-loading w2v model! Can take 15 minutes to 1 hour.")
-        print("Press Enter to continue when a txt file of 14GB file with the same name 
+        print("Press Enter to continue when a txt file of 14GB file with the same name.")
         model = KeyedVectors.load_word2vec_format('word2vec_twitter_tokens.bin', binary=True, unicode_errors='ignore')
         model.save_word2vec_format('word2vec_twitter_tokens.txt', binary=False)
 
