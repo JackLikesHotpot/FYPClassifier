24d99e7c7e3bcc7aa492f829454e5eedd5f40186
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-05-10 05:19:40 +0100

    Update prepareW2V.py

--- prepareW2V.py
+++ prepareW2V.py
@@ -3,14 +3,14 @@ from gensim.models import Word2Vec
 import numpy as np
 
 
-def prepareW2V(model, data):
+def prepareW2V(model, data):                                    # code to produce a mean vector for the model and a tweet
     p = []
     for tweet in data:
         p.append(get_mean_vector(model, tweet))
     return p
 
 
-def get_mean_vector(word2vec_model, words):
+def get_mean_vector(word2vec_model, words):                     # mean vector of the embeddings
     words = [word for word in words if word in word2vec_model.wv.vocab]
     if len(words) >= 1:
         return np.mean(word2vec_model[words], axis=0)
